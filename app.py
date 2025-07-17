a394b52daaf7af2763dd1af93b4d2220f2d2310a
Md Aminul Islam Bhuiyan <aminulamin0001@gmail.com>
2025-07-17 15:49:13 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+import torch
+import numpy as np
+from model import MyNN
+
+app = Flask(__name__)
+
+# Load model
+model = MyNN(num_features=784)
+model.load_state_dict(torch.load("fashion_ann.pth", map_location=torch.device('cpu')))
+model.eval()
+
+@app.route("/")
+def home():
+    return "Fashion MNIST ANN API is running!"
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        data = request.get_json()
+        input_data = np.array(data["input"]).reshape(1, 784) / 255.0
+        input_tensor = torch.tensor(input_data, dtype=torch.float32)
+        output = model(input_tensor)
+        prediction = torch.argmax(output, dim=1).item()
+        return jsonify({"prediction": prediction})
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,15 @@
+import torch.nn as nn
+
+class MyNN(nn.Module):
+    def __init__(self, num_features):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(num_features, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, 10)
+        )
+
+    def forward(self, x):
+        return self.model(x)
